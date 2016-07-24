90ebe640cbdf8e618e0f5f4dc694b9e20aeab7dd
Ricardo Erikson <ricardoerikson@gmail.com>
2016-07-24 01:26:23 -0400

    Added mocha and chai tests

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var starWarsNames = require('./index.js');
+
+describe('starwars-names-lib', function () {
+    it('should work', function () {
+        expect(true).to.be.true;
+    });
+});
