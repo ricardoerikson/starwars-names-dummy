38110fa9313aa797be5bdb077234cb74e143665d
Ricardo Erikson <ricardoerikson@gmail.com>
2016-07-24 20:39:20 -0400

    Adding unit tests with mocha and chai

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,24 @@ var expect = require('chai').expect;
 var starWarsNames = require('./index.js');
 
 describe('starwars-names-lib', function () {
-    it('should work', function () {
-        expect(true).to.be.true;
+    describe('all', function () {
+        it('should be an array of strings', function () {
+            expect(starWarsNames.all).to.satisfy(isArrayOfStrings);
+            function isArrayOfStrings(array) {
+                return array.every(function(item){
+                    return typeof item === 'string';
+                });
+            }
+        });
+        it('should contain `Luke Skywalker`', function () {
+            expect(starWarsNames.all).to.contain('Luke Skywalker');
+        });
+    });
+
+    describe('random', function () {
+        it('should return a random item from starWarsNames.all', function () {
+            var item = starWarsNames.random();
+            expect(starWarsNames.all).to.include(item);
+        });
     });
 });
